acdefaa2182ba7c141c467770a4a82d35d7f184d
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-07-28 19:13:32 +0530

    Time: 53 ms (100%), Space: 41.6 MB (100%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
@@ -1,26 +1,28 @@
 class Solution {
-    public int nonSpecialCount(int l, int r) {
-        int specialCount = 0;
-        int sqrtL = (int) Math.ceil(Math.sqrt(l));
-        int sqrtR = (int) Math.floor(Math.sqrt(r));
-        while (sqrtL <= sqrtR) {
-            if (isPrime(sqrtL++)) {
-                specialCount++;
-            }
-        }
-        return (r + 1 - l) - specialCount;
-    }
-
-    private boolean isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2 || num == 3) return true;
-        if (num % 2 == 0 || num % 3 == 0) return false;
-        int sqrt = (int) Math.sqrt(num);
-        for (int i = 5; i <= sqrt; i++) {
-            if (num % i == 0) {
-                return false;
-            }
+    public boolean checkPrime(int n){
+           int sqrt=(int)Math.sqrt(n);
+        if(n<=1)return false;
+        if(n==3||n==2)return true;
+        if(n%2==0||n%3==0)return false;
+        for(int i=5;i<=sqrt;i++){
+            if(n%i==0)return false;
         }
         return true;
+    }
+    public int nonSpecialCount(int l, int r) {
+        // int sqrt=(int)Math.sqrt(r);
+
+          int l2=(int)Math.ceil(Math.sqrt(l));
+          int r2=(int)Math.floor(Math.sqrt(r));
+int count=0;//for special
+          while(l2<=r2){
+            if(checkPrime(l2)){
+count++;
+
+            }
+l2++;
+          }
+          return (r+1-l)-count;
+
     }
 }
