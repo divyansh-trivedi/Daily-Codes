8467b0736eec9a2628da76c24ff5936e4d42769d
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-08-06 21:42:21 +0530

    Time: 65 ms (24.42%), Space: 45.6 MB (45.19%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char a : word.toCharArray()) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+
+        // Convert map entries to a list and sort by frequency using a lambda
+        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+        int ans = 0;
+        int t = 0;
+        int r = 1;
+        for (Map.Entry<Character, Integer> entry : entryList) {
+            ans += entry.getValue() * r;
+            t++;
+            if (t == 8) {
+                r += 1;
+                t = 0;
+            }
+        }
+        return ans;
+    }
+
+}
