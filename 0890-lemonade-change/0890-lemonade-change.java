9f923e34fb3d0aad19a497c47d360af4b2722ed1
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-08-15 13:17:02 +0530

    Time: 286 ms (5.52%), Space: 55.8 MB (48.09%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,30 +1,46 @@
 class Solution {
-    // different than gas station, as the starting point is fixed
-    // and bills have different nomination, 2 $10s cannot be used as $15 change
-
     public boolean lemonadeChange(int[] bills) {
-        int fiveCnt = 0, tenCnt = 0;
-        
-        for (int i = 0; i < bills.length; i++) {
-            if (bills[i] == 5) {
-                fiveCnt++;
-            } else if (bills[i] == 10) {
-                tenCnt++;
-                fiveCnt--;
-            } else {
-                if (tenCnt > 0) {
-                    tenCnt--; // if have 10, use 10 first, as 5 is more flexble.
-                } else {
-                    fiveCnt -= 2; // use five to pay 10 change.
-                }
+        int a=0,b=0,c=0;
+boolean flag=true;
+if(bills[0]>5)return false;
+ 
+ 
 
-                fiveCnt--; // use to pay the rest 5 change.
+        for(int i=0;i<bills.length;i++){
+         
+
+           if(bills[i]==5){
+            a++;
+            
+           } 
+           else if(bills[i]==10){
+
+            if(a==0)
+            return false;
+
+            a--;
+            b++;
+           }
+           else{
+            
+            
+             if(a>=1&&b>=1 ){
+                a--;
+                b--;
+              
+            }else if(a>=3 ){
+            a=a-3;
+           
             }
+            else{
+            return false;
 
-            if (fiveCnt < 0) return false;
+            }
+           }
+
         }
 
         return true;
-
+        
     }
 }
