24965bfc71cddb15fcbadc1b2794500da9a3e429
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-09-11 17:59:58 +0530

    Time: 7 ms (8.14%), Space: 47.5 MB (16.43%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,45 +1,33 @@
 import java.util.*;
 class Solution {
  
-    // public int solve(int dp[][],int[][] grid, int i, int j){
-    //    int m=grid.length;
-    //    int n=grid[0].length;
-
-    // if(i>=m || j>=n) return Integer.MAX_VALUE;
-    // if(i==m-1&& j==n-1)
-    // return grid[i][j];
-    // if(dp[i][j]!=-1)return dp[i][j];
-
-
-    // int down=solve(dp,grid, i+1,j);
-    // int right=solve(dp,grid,i,j+1);
-
-    // return dp[i][j]=grid[i][j]+Math.min(down,right);
-
-    // }
-
     public int minPathSum(int[][] ar) {
          int m=ar.length;
          int n=ar[0].length;
-        int dp[][]= new int[m][n];
-       dp[0][0]=ar[0][0];
-       
+      int prev[]= new int[n];
+     
        for(int i=0;i<m;i++){
+        int curr[]= new int[n];
+       
         for(int j=0;j<n;j++){
-            if(i==0&&j==0)continue;
+            if(i==0&&j==0)curr[j]=ar[0][0];
         
             else{
                 int up=ar[i][j],left=ar[i][j];
-                if(i>0)up+=dp[i-1][j];
+                // requires previous row's j coloumn
+                if(i>0)up+=prev[j];
                 else up+=1e9;
-                if(j>0)left+=dp[i][j-1];
+                // requires current row's j-1 coloumn
+                if(j>0)left+=curr[j-1];
                 else
                 left+=1e9;
-                dp[i][j]=Math.min(up,left);
+                curr[j]=Math.min(up,left);
             }
+            
         }
+        prev=curr;
         
        }
-       return dp[m-1][n-1];
+       return prev[n-1];
     }
 }
