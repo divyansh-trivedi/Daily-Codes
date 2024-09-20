ea0828cd6e710a79381d619d3203b1f39020a94e
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-09-20 19:03:07 +0530

    Time: 2 ms (87.89%), Space: 55.2 MB (79.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -9,19 +9,14 @@ class Solution {
 
        int ar[]= new int[nums.length];
        for(int i=0;i<nums.length;i++){
-       if (count_zero > 1) {
-                // If there are two or more zeros, every element should be 0
-                ar[i] = 0;
-            } else if (nums[i] == 0) {
-                // If there's exactly one zero, place the product at the zero's position
-                ar[i] = prod;
-            } else if (count_zero == 0) {
-                // If there are no zeros, divide the total product by the current element
-                ar[i] = prod / nums[i];
-            } else {
-                // For all non-zero elements when there is exactly one zero, the result is 0
-                ar[i] = 0;
-            }
+        if(count_zero>1)
+        ar[i]=0;
+        else if(nums[i]==0)
+        ar[i]=prod;
+        else if(count_zero==0)
+        ar[i]=prod/nums[i];
+        else
+        ar[i]=0;
        }
 
        return ar; 
