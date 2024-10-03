3b0e799272dadb81834baea9d68c335da6ffdf37
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-10-03 23:12:10 +0530

    Time: 23 ms (92.02%), Space: 56.5 MB (61.03%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
@@ -1,30 +1,24 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-     long ts=0;
-     for(int i:nums)ts+=i;
-
-     int rem=(int)(ts%p);
-     if(rem==0)return 0;
-
-     HashMap<Integer,Integer> prefixMod= new HashMap<>();
-     prefixMod.put(0,-1);
-     long prefixSum=0;
-     int min=nums.length;
-     for(int i=0;i<nums.length;i++){
-        prefixSum+=nums[i];
-        int curr=(int)(prefixSum%p);
-        int target=(curr-rem+p)%p;
-
-        if(prefixMod.containsKey(target))
-        min=Math.min(min,i-prefixMod.get(target));
-
-
-        prefixMod.put(curr,i);
-
-
-     }
-
-     return min==nums.length?-1:min;
-
+      int n  = nums.length;
+      int ans = 0;
+      for(int i=0;i<n;i++){
+          ans = (ans+nums[i])%p;
+      }   
+      if( ans == 0) return 0;
+      int res =n;
+      int sum =0;
+      HashMap<Integer, Integer> map = new HashMap<>();
+      map.put(0,-1);
+      for(int i=0;i<n;i++){
+          sum = (sum+ nums[i]) %p;
+         
+          int mod = ( sum-ans+p) %p;
+           map.put(sum,i);
+          if(map.containsKey(mod)){
+              res = Math.min( res, i-map.get(mod));
+          }
+      }
+      return res < n  ? res: -1;
     }
 }
