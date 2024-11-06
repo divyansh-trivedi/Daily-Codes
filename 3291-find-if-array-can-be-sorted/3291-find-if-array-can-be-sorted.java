202bb3f800793178700d49f59881e3159b5c13fc
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-11-06 23:21:59 +0530

    Time: 1 ms (100%), Space: 44.3 MB (61.64%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        int numOfSetBits = Integer.bitCount(nums[0]);
+        int maxOfSegment = nums[0];
+        int minOfSegment = nums[0];
+        int maxOfPrevSegment = Integer.MIN_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (Integer.bitCount(nums[i]) == numOfSetBits) {
+                maxOfSegment = Math.max(maxOfSegment, nums[i]);
+                minOfSegment = Math.min(minOfSegment, nums[i]);
+            } else { 
+                if (minOfSegment < maxOfPrevSegment) {
+                    return false;
+                }
+                maxOfPrevSegment = maxOfSegment;
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
+                numOfSetBits = Integer.bitCount(nums[i]);
+            }
+        }
+        if (minOfSegment < maxOfPrevSegment) {
+            return false;
+        }
+        
+        return true;
+    }
+}
