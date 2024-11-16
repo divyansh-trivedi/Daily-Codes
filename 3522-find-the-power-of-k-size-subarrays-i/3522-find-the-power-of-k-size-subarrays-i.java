47b605d855995d92631b9e0d92334844c187eea6
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-11-16 22:27:26 +0530

    Time: 2 ms (58.78%), Space: 45.5 MB (27.66%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int solve(int ar[], int i, int k){
-        for(int j=0;j<k-1;j++){
-          if(ar[i+j]!=ar[i+j+1]-1)return -1;
-        }
-
-        return ar[i+k-1];
-    }
     public int[] resultsArray(int[] nums, int k) {
-        int n=nums.length;
-        int ar[]= new int [n-k+1];
-        for(int i=0;i<=n-k;i++){
-            ar[i]=solve(nums,i,k);
-        }
-        return ar;
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        for(int i=0;i<n-k+1;i++){
+         
+            int ele=nums[i+k-1];// subaaray ka last assign kr do
+            for(int j=i;j<i+k-1;j++){
+                if(nums[j+1]-nums[j]!=1){
+                    ele = -1;
+                    break;
+                }
+                }
+                res[i]=ele;
+            }
+        
+        return res;
     }
 }
