4667b9ae28b879f0fe638065842c944162ee5250
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2024-12-06 20:00:34 +0530

    Time: 26 ms (36.87%), Space: 45.5 MB (54.77%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,29 +1,37 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        int i1 = 0;
-        int i2 = 0;
-        while (true) {
-            while(i1 < target.length() && target.charAt(i1) == '_') {
-                i1++;
-            }
-            while(i2 < start.length() && start.charAt(i2) == '_') {
-                i2++;
-            }
-            if (i1 == target.length() || i2 == start.length()) {
-                break;
-            }
-            if (target.charAt(i1) != start.charAt(i2)) {
-                return false;
-            }
-            if (target.charAt(i1) == 'L' && i1 > i2) {
-                return false;
-            }
-            if (target.charAt(i1) == 'R' && i1 < i2) {
-                return false;
-            }
-            i1++;
-            i2++;
+       int cnt1=0;
+       for(int i:start.toCharArray()){
+        if(i!='_')cnt1++;
+       }
+       for(int i:target.toCharArray()){
+        if(i!='_')cnt1--;
+       }
+        if(cnt1!=0)return false;
+        
+        int i=0, j=0;
+           
+        while(i<start.length()&&j<target.length()){
+
+            while(i<start.length()&& start.charAt(i)=='_')i++;
+            while(j<target.length()&& target.charAt(j)=='_')j++;
+
+            if(i==start.length() || j==target.length())
+            return start.length()==i&&target.length()==j;
+
+            
+            char c1 = start.charAt(i);
+            char c2 = target.charAt(j);
+            
+
+            if(c1!=c2)return false;
+            else if(c1=='L'&&i<j)return false;
+            else if(c2=='R'&&i>j)return  false;
+
+            i++;
+            j++;
+
         }
-        return i1 == i2;
+        return true;
     }
 }
