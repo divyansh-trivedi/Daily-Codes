f30a4b2b4eb8fdcae83931fa19be228c74431c58
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-02-16 12:00:49 +0530

    Time: 8 ms (35.86%), Space: 45.3 MB (63.61%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,34 +1,27 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int dp[][] = new int[n][n+1];
+        int dp[][] = new int[n][n];
 
-        int min = (int)1e9;
-        for(int j = 0;j<n;j++){
+        int min = (int) 1e9;
+        for (int j = 0; j < n; j++) {
             dp[0][j] = matrix[0][j];
-            min = Math.min(min ,matrix[0][j]);
         }
 
-        dp[0][n] = min;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int left = (j - 1 >= 0) ? dp[i - 1][j - 1] + matrix[i][j] : (int) 1e9;
+                int right = (j + 1 < n) ? dp[i - 1][j + 1] + matrix[i][j] : (int) 1e9;
+                int down = dp[i - 1][j] + matrix[i][j];
 
-        for(int i = 1 ;i <n;i++){
-            min = (int)1e9;
-            for(int j = 0;j<n;j++){
-                int left , right , down;
-                left =right =down =(int)1e9;
-
-                if(j-1 >= 0) left = dp[i-1][j-1] + matrix[i][j];
-                if(j+1 < n) right = dp[i-1][j+1] + matrix[i][j];
-                down = dp[i-1][j] + matrix[i][j];
-
-                dp[i][j] = Math.min(left , Math.min(right , down));
-
-                min = Math.min(min , dp[i][j]);
+                dp[i][j] = Math.min(left, Math.min(right, down));
             }
-            dp[i][n] = min;
         }
-        return dp[n-1][n];
 
-
+        int result = (int) 1e9;
+        for (int j = 0; j < n; j++) {
+            result = Math.min(result, dp[n - 1][j]);
+        }
+        return result;
     }
 }
