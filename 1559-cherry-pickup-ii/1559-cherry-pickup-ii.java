dfd683b57d9b67b705f908c8f1082a84a1651587
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-02-17 10:05:00 +0530

    Time: 42 ms (16.27%), Space: 45.9 MB (71.26%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,32 +1,29 @@
 class Solution {
-    int r, c;
-
     public int cherryPickup(int[][] grid) {
-        r = grid.length;
-        c = grid[0].length;
+        int r = grid.length;
+        int c = grid[0].length;
         int dp[][][] = new int[r][c][c];
 
-        for (int j1 = 0; j1 < c; j1++) {
-            for (int j2 = 0; j2 < c; j2++) {
-                dp[r - 1][j1][j2] = (j1 == j2) ? grid[r - 1][j1] : grid[r - 1][j1] + grid[r - 1][j2];
-            }
-        }
+        for(int j1=0;j1<c ;j1++)
+            for(int j2 =0;j2<c;j2++)
+                dp[r-1][j1][j2] = (j1 == j2)? grid[r-1][j1] : grid[r-1][j1] + grid[r-1][j2];
+        
+        for(int i =r-2;i>=0;i--){
+            for(int j1 = 0;j1 < c;j1++){
+                for(int j2 =0;j2<c;j2++){
 
-        for (int i = r - 2; i >= 0; i--) {
-            for (int j1 = 0; j1 < c; j1++) {
-                for (int j2 = 0; j2 < c; j2++) {
-                    int max = -(int) 1e7;
+                    int max = -(int)1e9;
+                    for(int dj1 = -1;dj1 <2 ; dj1++){
+                        for(int dj2 = -1;dj2 <2 ; dj2++){
+                            int newj1 = j1 + dj1;
+                            int newj2 = j2 + dj2;
 
-                    for (int di = -1; di <= 1; di++) {
-                        for (int dj = -1; dj <= 1; dj++) {
-                            int newJ1 = j1 + di;
-                            int newJ2 = j2 + dj;
-
-                            if (newJ1 >= 0 && newJ1 < c && newJ2 >= 0 && newJ2 < c) {
-                                int value = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-                                value += dp[i + 1][newJ1][newJ2];
-                                max = Math.max(max, value);
+                            if(newj1 >= 0 && newj1 < c && newj2 >=0 && newj2 <c){
+                                int value = (j1 == j2)? grid[i][j1] : grid[i][j1] + grid[i][j2];
+                                value += dp[i+1][newj1][newj2];
+                                max= Math.max(max, value);
                             }
+
                         }
                     }
 
@@ -34,7 +31,7 @@ class Solution {
                 }
             }
         }
-
-        return dp[0][0][c - 1];
+         return dp[0][0][c-1] ;  
+        
     }
 }
