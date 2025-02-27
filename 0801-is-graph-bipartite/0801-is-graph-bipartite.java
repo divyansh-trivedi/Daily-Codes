50ce296450394ddfecd54efe10945f5ed37fb03b
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-02-27 18:08:10 +0530

    Time: 1 ms (63.27%), Space: 44.9 MB (78.31%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,26 +1,41 @@
+import java.util.*;
+
+public class BipartitieGraph {
+    public static void main(String[] args) {
+        int[][] graph = {
+            {1, 3},
+            {0, 2},
+            {1, 3},
+            {0, 2}
+        };
+        Solution solution = new Solution();
+        System.out.println(solution.isBipartite(graph));
+    }
+}
+
 class Solution {
-    public boolean bfs(int vis[], int start , int [][] graph){
-        Queue<Integer> q= new LinkedList<>();
+    public boolean bfs(int vis[], int start, int[][] graph) {
+        Queue<Integer> q = new LinkedList<>();
         vis[start] = 0;
         q.add(start);
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int node = q.poll();
-            for(int it : graph[node]){
-                if(vis[it] == -1){
+            for (int it : graph[node]) {
+                if (vis[it] == -1) {
                     vis[it] = vis[node] == 1 ? 0 : 1;
                     q.add(it);
-                }else if(vis[it] == vis[node])return false;
+                } else if (vis[it] == vis[node]) return false;
             }
         }
         return true;
     }
+
     public boolean isBipartite(int[][] graph) {
         int vis[] = new int[graph.length];
-        Arrays.fill(vis,-1);
-
-        for(int i=0;i<vis.length;i++){
-            if(vis[i] == -1)
-                if(!bfs(vis, i , graph))return false;
+        Arrays.fill(vis, -1);
+        for (int i = 0; i < vis.length; i++) {
+            if (vis[i] == -1)
+                if (!bfs(vis, i, graph)) return false;
         }
         return true;
     }
