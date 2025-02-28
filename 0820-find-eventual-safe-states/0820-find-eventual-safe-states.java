eea62935dbdcc95e9b6022f3235fae69bb501dbb
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-02-28 10:38:58 +0530

    Time: 12 ms (52.94%), Space: 56.2 MB (8.09%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,39 +1,48 @@
-import java.util.*;
-
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        Set<Integer> safeNodes = new HashSet<>();
-        List<Integer> result = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (graph[i].length == 0) {
-                safeNodes.add(i);
-            }
-        }
-
-        boolean changed;
-        do {
-            changed = false;
-            for (int i = 0; i < n; i++) {
-                if (!safeNodes.contains(i)) {
-                    boolean isSafe = true;
-                    for (int neighbor : graph[i]) {
-                        if (!safeNodes.contains(neighbor)) {
-                            isSafe = false;
-                            break;
-                        }
-                    }
-                    if (isSafe) {
-                        safeNodes.add(i);
-                        changed = true;
-                    }
+    public boolean dfs(int pathVis[],int node , int vis[], ArrayList<ArrayList<Integer>> adj, int check[]){
+                vis[node] =1;
+                pathVis[node] =1;
+                check[node] =0;
+        
+                for(int i: adj.get(node)){
+                    if(vis[i] == 0){
+                        if(dfs(pathVis, i, vis, adj, check)==true)return true;
+                    }else if (pathVis[i] == 1) return true;
                 }
+                check[node] = 1;
+                pathVis[node] = 0;
+                return false;
             }
-        } while (changed);
+        
+          
+            public List<Integer> safeNodes( ArrayList<ArrayList<Integer>> adj) {
+               int v = adj.size();
+               int vis[] = new int[v];
+               int pathVis[] = new int[v];
+               int check[] = new int[v];
+               List<Integer> list = new ArrayList<>();
+        
+               for (int i = 0; i < v; i++) {
+                if (vis[i] == 0) 
+                    dfs(pathVis, i, vis, adj, check);
+               }
+        
+               for(int i=0;i<v;i++){
+                if(check[i] == 1)list.add(i);
+               }
+               return list;
+            }
+        
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < graph.length; i++) 
+        list.add(new ArrayList<>()); 
 
-        result.addAll(safeNodes);
-        Collections.sort(result);
-        return result;
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++)
+                list.get(i).add(graph[i][j]);
+        }
+        return safeNodes(list);
     }
+    
 }
