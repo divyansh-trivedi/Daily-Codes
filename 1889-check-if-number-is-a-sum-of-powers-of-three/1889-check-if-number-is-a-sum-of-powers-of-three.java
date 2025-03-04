549d2cb2d5887e74e6e138ca1d0cefa224bb8ddb
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-03-04 12:13:41 +0530

    Time: 167 ms (6.98%), Space: 40.9 MB (9.3%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,17 +1,14 @@
 class Solution {
-    public boolean solve(int n , int ar[] , int idx, int curr){
+    public boolean solve(int n , int ar[] , int idx, int curr, boolean dp[]){
         if(curr == n)return true;
         if(idx >15)return false;
+        if(dp[idx] != false)return dp[idx];   
 
-        boolean pick = solve(n,ar,idx+1,curr+((int)Math.pow(3,ar[idx])));
-        boolean npick = solve(n , ar , idx+1, curr);
-
-        return pick || npick;
+        return dp[idx] = solve(n,ar,idx+1,curr+((int)Math.pow(3,ar[idx])), dp) || solve(n , ar , idx+1, curr, dp);
     }
     public boolean checkPowersOfThree(int n) {
-        int ar[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-
-       return solve(n , ar,0,0);
-         
+        int ar[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        boolean dp[] = new boolean[16];
+       return solve(n , ar,0,0,dp);
     }
 }
