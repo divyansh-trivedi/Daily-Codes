857d057e01fa7328425a7ad5c357ce85c01a63d7
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-03-20 16:03:12 +0530

    Time: 189 ms (11.34%), Space: 81.1 MB (29.37%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -1,66 +1,87 @@
-class Solution {
-    int[] xDir = {0,0,-1,1};
-    int[] yDir = {-1,1,0,0};
-    public int DFS(int[][] grid,int i,int j,int var)
-    {
-        grid[i][j] = var;
-        int count = 1;
-        for(int k = 0;k<4;k++)
-        {
-            int newRow = i+xDir[k];
-            int newCol = j+yDir[k];
-            if(newRow>=0 && newRow<grid.length && newCol>=0 && newCol<grid[0].length && grid[newRow][newCol] == 1)
-            {
-                count+=DFS(grid,newRow,newCol,var);
-            }
+class Disjoint {
+    List<Integer> size = new ArrayList<>(), parent = new ArrayList<>();
+
+    public Disjoint(int n) {
+        for (int i = 0; i < n; i++) {
+            size.add(1);
+            parent.add(i);
         }
-        return count;
     }
+
+    public int findParent(int node) {
+        if (node == parent.get(node)) return node;
+        parent.set(node, findParent(parent.get(node)));
+        return parent.get(node);
+    }
+
+    public void unionBySize(int u, int v) {
+        int pu = findParent(u), pv = findParent(v);
+        if (pu != pv) {
+            if (size.get(pu) < size.get(pv)) {
+                parent.set(pu, pv);
+                size.set(pv, size.get(pu) + size.get(pv));
+            } else {
+                parent.set(pv, pu);
+                size.set(pu, size.get(pu) + size.get(pv));
+            }
+        }
+    }
+}
+ class Solution {
     public int largestIsland(int[][] grid) {
-        int islands = 0;
-        int var = -1;
-        HashMap<Integer,Integer> hmap = new HashMap<>();
-        for(int i = 0;i<grid.length;i++)
-        {
-            for(int j = 0;j<grid[0].length;j++)
-            {
-                if(grid[i][j] == 1)
-                {
-                    int area = DFS(grid,i,j,var);
-                    hmap.put(var,area);
-                    var--;
+        int n = grid.length;
+        Disjoint ds = new Disjoint (n*n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 0)continue;
+                int dr[] = {-1,0,1,0};
+                int dc[] ={0,1,0,-1};
+
+                for(int k=0;k<4;k++){
+                    int newr = i + dr[k];
+                    int newc = j + dc[k];
+
+                    if(newr>=0&&newc>=0&&newr<n&&newc<n  && grid[newr][newc] == 1){
+                        int nodeNo = i*n + j;
+                        int newNodeNo = newr*n + newc;
+
+                        ds.unionBySize(nodeNo , newNodeNo);
+                    }
                 }
             }
         }
-        boolean zero = false;
-        int maxArea = Integer.MIN_VALUE;
-        for(int i = 0;i<grid.length;i++)
-        {
-            for(int j = 0;j<grid[0].length;j++)
-            {
-                if(grid[i][j] == 0)
-                {
-                    zero = true;
-                    HashSet<Integer> hset = new HashSet<>();
-                    int area = 1;
-                    for(int k = 0;k<4;k++)
-                    {
-                        int newRow = i+xDir[k];
-                        int newCol = j+yDir[k];
-                        if(newRow >= 0 && newRow<grid.length && newCol >= 0 && newCol<grid[0].length)
-                        {
-                            hset.add(grid[newRow][newCol]);
-                        }
+
+        int max = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1) continue;
+                Set<Integer> components = new HashSet<>();
+
+                int dr[] = {-1,0,1,0};
+                int dc[] ={0,1,0,-1};
+
+                for(int k=0;k<4;k++){
+                    int newr = i+dr[k];
+                    int newc = j + dc[k];
+
+                    if(newr>=0&&newc>=0&&newr<n&&newc<n ){
+                        if(grid[newr][newc] == 1)
+                        components.add(ds.findParent(newr*n+newc));
                     }
-                    for(int item : hset)
-                    {
-                        if(item != 0)
-                            area += hmap.get(item);
-                    }
-                    maxArea = Math.max(maxArea,area);
                 }
-            }   
+                int total_size = 0;
+                
+                for(Integer it : components){
+                    total_size += ds.size.get(it);
+                }
+                max = Math.max(max, total_size + 1);
+            }
         }
-        return (zero == true)?maxArea:hmap.get(-1);
+        for(int i=0;i<n*n;i++){
+            max= Math.max(max, ds.size.get(ds.findParent(i)));
+        }
+        return max;
     }
 }
