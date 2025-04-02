51ef6fd273f337092ba3682a96a884c731bec7df
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-02 11:34:49 +0530

    Time: 5 ms (64.85%), Space: 45.5 MB (7.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,19 @@
-import java.util.*;
 class Solution {
-    public int solve(int dp[][],String word1,String word2, int i, int j){
-        // BASE CASES
-        if(i>=word1.length())return  word2.length()-j;
+    public int minDistance(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int dp[][] = new int[n+1][m+1];
 
-        if(j>=word2.length()) return word1.length()-i;
+        for(int i=0;i<=n;i++)dp[i][0] = i;
+        for(int j=0;j<=m;j++)dp[0][j] = j;
         
-        if(dp[i][j]!=-1)return dp[i][j];
-       
-
-        if(word1.charAt(i)==word2.charAt(j))
-        return dp[i][j] =solve (dp,word1, word2, i+1, j+1);
-      
-        int update=1+solve(dp,word1,word2, i+1, j+1);
-        int delete=1+solve(dp,word1, word2, i+1,j);
-        int replace= 1+solve(dp,word1, word2,i, j+1);
-
-        return dp[i][j]= Math.min(update, Math.min(delete,replace));
-    }
-    public int minDistance(String word1, String word2) {
-        int n=word1.length();int m=word2.length();
-        int dp[][]= new int[n+1][m+1];
-for(int row[]:dp)
-Arrays.fill(row,-1);
-
-    return solve (dp,word1, word2, 0,0);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                dp[i][j] = dp[i-1][j-1];
+                else 
+                dp[i][j] = Math.min(dp[i-1][j-1],Math.min( dp[i-1][j], dp[i][j-1]))+1;
+            }
+        }
+        return dp[n][m];
     }
 }
