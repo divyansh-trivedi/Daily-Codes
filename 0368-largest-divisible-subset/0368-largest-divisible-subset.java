aefd2d4d1b26fd01169e2189aa537b75467da898
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-10 18:58:10 +0530

    Time: 13 ms (98.69%), Space: 42.8 MB (97.06%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,30 +1,32 @@
 class Solution {
-    public List<Integer> largestDivisibleSubset(int[] arr) {
-        Arrays.sort(arr);
-        int max =1 , n = arr.length;
-        int lastIndex =0;
-        int dp[] =  new int[n];
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int dp[] = new int[n];
         Arrays.fill(dp,1);
         int hash[] = new int[n];
-        for(int i=0;i<n;i++){
+
+        int max =1 , lastIndex = 0;
+        for(int i=1;i<n;i++){
             hash[i] = i;
-            for(int prev=0;prev<i;prev++){ 
-                if(arr[i]%arr[prev] == 0 && 1+dp[prev] > dp[i]){
-                    dp[i] = 1+ dp[prev];
-                    hash[i] = prev;
-             } 
-             if(dp[i]>max){
+            for(int j=0;j<i;j++){
+                if(nums[i] %nums[j] == 0 && dp[j]+1 > dp[i]){
+                    dp[i] = dp[j]+1;
+                    hash[i] = j;
+                }
+            }
+            if(dp[i] > max){
                 max = dp[i];
                 lastIndex = i;
-             }
             }
         }
-
         List<Integer> list = new ArrayList<>();
-        list.add(arr[lastIndex]);
-        while(hash[lastIndex]!=lastIndex){
+        
+        list.add(nums[lastIndex]);
+
+        while(lastIndex != hash[lastIndex]){
             lastIndex = hash[lastIndex];
-            list.add(arr[lastIndex]);
+            list.add(nums[lastIndex]);
         }
         Collections.reverse(list);
         return list;
