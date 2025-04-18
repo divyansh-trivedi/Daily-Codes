e1a85fc5c5508c95f6265d3c0b2591012491eb74
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-18 15:49:07 +0530

    Time: 1 ms (99.86%), Space: 48.2 MB (5.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,33 +1,28 @@
-import java.util.*;
 class Solution {
- 
-    public int minPathSum(int[][] ar) {
-         int m=ar.length;
-         int n=ar[0].length;
-      int prev[]= new int[n];
-     
-       for(int i=0;i<m;i++){
-        int curr[]= new int[n];
-       
-        for(int j=0;j<n;j++){
-            if(i==0&&j==0)curr[j]=ar[0][0];
-        
-            else{
-                int up=ar[i][j],left=ar[i][j];
-                // requires previous row's j coloumn
-                if(i>0)up+=prev[j];
-                else up+=1e9;
-                // requires current row's j-1 coloumn
-                if(j>0)left+=curr[j-1];
-                else
-                left+=1e9;
-                curr[j]=Math.min(up,left);
-            }
-            
+private int solve(int[][] grid,int[][] dp,int i,int j){
+    if(i<0 || j<0){
+        return Integer.MAX_VALUE;
+    }
+    if(i==0 && j==0){
+        return grid[i][j];
+    }
+    
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int up =  solve(grid,dp,i-1,j);
+    int left = solve(grid,dp,i,j-1);
+    int sum1 = (up == Integer.MAX_VALUE)?Integer.MAX_VALUE : up + grid[i][j];
+    int sum2 = (left == Integer.MAX_VALUE)?Integer.MAX_VALUE : left + grid[i][j];
+    return dp[i][j] = Math.min(sum1,sum2);
+}
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        prev=curr;
-        
-       }
-       return prev[n-1];
+        return solve(grid,dp,m-1,n-1);
     }
 }
