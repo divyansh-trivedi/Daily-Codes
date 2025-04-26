76e94df83a661e689038972df7cb6674394894ea
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-26 16:45:50 +0530

    Time: 8 ms (77.44%), Space: 59.5 MB (29.83%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,59 +1,46 @@
 class Solution {
-     private static int  solve(int[][] grid){
-        Queue<Pair> q = new LinkedList<>();
+    private static int dx[]={-1, 0,1,0};
+    private static int dy[]={0, 1,0,-1};
+    private void dfs(int grid[][],boolean vis[][], int i, int j){
         int n= grid.length;
-        int m=grid[0].length;
+        int m= grid[0].length;
+        vis[i][j] = true;
+
+        for(int k=0;k<4;k++){
+            int nr = i+dx[k];
+            int nc = j+dy[k];
+
+            if(nr>=0&&nc>=0&&nr<n&&nc<m){
+                if(!vis[nr][nc] && grid[nr][nc]==1){
+                    dfs(grid,vis,nr, nc);
+                }
+            }
+        }
+    }
+    public int numEnclaves(int[][] grid) {
+        int n= grid.length , m= grid[0].length;
         boolean vis[][] = new boolean[n][m];
 
         for(int i=0;i<n;i++){
-            for (int j = 0; j < m; j++) {
-                if((i==0 || i==n-1||j==0||j==m-1) && grid[i][j] == 1){
-                    q.add(new Pair(i, j));
-                    vis[i][j] = true;
-                }
-            }
+            if(grid[i][0] == 1)
+            dfs(grid, vis, i ,0);
+            if(grid[i][m-1] == 1)
+            dfs(grid, vis, i , m-1);
         }
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,1,0,-1};
-
-        while(!q.isEmpty()){
-            Pair rem = q.poll();
-            int row = rem.r;
-            int col = rem.c;
-
-            for(int k=0;k<4;k++){
-                int nr = row + dr[k];
-                int nc = col + dc[k];
-                
-                if(nr>=0&&nr<n && nc>=0 && nc<m && !vis[nr][nc]){
-                    if(grid[nr][nc] == 1){
-                       vis[nr][nc] = true;
-                       q.add(new Pair(nr, nc));
-                     }else continue;
-
-                }
-            }        
+        for(int i=0;i<m;i++){
+            if(grid[0][i] == 1)
+            dfs(grid, vis, 0 ,i);
+            if(grid[n-1][i] == 1)
+            dfs(grid, vis, n-1 , i);
         }
 
-        int cnt =0;
+        int cnt=0;
 
         for(int i=0;i<n;i++){
-            for (int j = 0; j < m; j++) {
-                if(!vis[i][j] && grid[i][j] == 1)cnt++;
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1&& !vis[i][j])cnt++;
             }
         }
         return cnt;
-
-    }
-    public int numEnclaves(int[][] grid) {
-        return solve(grid);
-    }
-}
-class Pair {
-    int r;
-    int c;
-    Pair(int r, int c){
-        this.r= r;
-        this.c =c;
-    }
+     }
 }
