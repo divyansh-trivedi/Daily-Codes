5c4e64081acdc7492e154b00c26348f6ce1fede7
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-26 13:04:09 +0530

    Time: 2 ms (68.62%), Space: 42.3 MB (75.62%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,59 +1,56 @@
-class Solution {
-    class Pair{
-        int row ;
-        int col; int time;
-        Pair(int r, int c , int t){
-            row = r; col=c ; time =t;
-        }
+class Pair{
+    int row,col, time;
+    Pair(int r, int c, int t){
+        row = r;
+        col = c;
+        time = t;
     }
+}
+class Solution {
     public int orangesRotting(int[][] grid) {
         int n= grid.length;
         int m= grid[0].length;
 
         Queue<Pair> q = new LinkedList<>();
         int vis[][] = new int[n][m];
-        
-        int cntFresh =0 ;
+        int cntFresh =0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j] =2;
-                }else 
-                    vis[i][j]=0;
-
-                if(grid[i][j]==1)
-                    cntFresh++;
+                    q.add(new Pair(i, j, 0));
+                    vis[i][j] = 2;
+                }
+                
+                 if(grid[i][j]==1)cntFresh++;
             }
         }
 
         int time =0;
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,1,0,-1};
+        int dx[] ={-1,0,1,0};
+        int dy[] ={0,1,0,-1};
 
-            int cnt =0;
+        int cnt =0;
         while(!q.isEmpty()){
             Pair rem = q.poll();
-            int r = rem.row;
-            int c = rem.col;
-            int t = rem.time;
-
-            time = Math.max(t, time);
-
+            int r= rem.row;
+            int c =rem.col;
+            int t= rem.time;
+time = Math.max(time, t);
             for(int k=0;k<4;k++){
-                int newRow = dr[k] + r;
-                int newCol = dc[k] + c;
+                int newr = r+dx[k];
+                int newc = c+dy[k];
+                
+                if(newr<0 || newc<0 || newr>=n ||newc>=m)continue;
 
-                if(newRow < 0|| newRow >= n || newCol <0 || newCol >= m)continue;
-                if(grid[newRow][newCol] == 1 && vis[newRow][newCol] != 2){
-                    q.add(new Pair(newRow, newCol , t +1));
-                    vis[newRow][newCol] = 2;
+                if(grid[newr][newc] ==1&&vis[newr][newc]!=2){
+                    q.add(new Pair(newr, newc, t+1));
+                    vis[newr][newc] = 2;
                     cnt++;
                 }
             }
         }
+        if(cnt!=cntFresh)return -1;
+        else return time;
 
-        if(cntFresh != cnt)return -1;
-        return time;
     }
 }
