11cfb887bfef47a7e1c7b76a784ecb6a0c1a2042
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-26 12:14:51 +0530

    Time: 2 ms (50.1%), Space: 47.3 MB (76.26%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,43 +1,31 @@
-class Disjoint {
-    List<Integer> size = new ArrayList<>(), parent = new ArrayList<>();
-    public Disjoint(int n) {
-        for (int i = 0; i <= n; i++) {
-            size.add(0);
-            parent.add(i);
-        }
-    }
-    public int findParent(int node) {
-        if (node == parent.get(node)) return node;
-        parent.set(node, findParent(parent.get(node)));
-        return parent.get(node);
-    }
-    public void unionBySize(int u, int v) {
-        int pu = findParent(u), pv = findParent(v);
-        if (pu != pv) {
-            if (size.get(pu) < size.get(pv)) {
-                parent.set(pu, pv);
-                size.set(pv, size.get(pu) + size.get(pv));
-            } else {
-                parent.set(pv, pu);
-                size.set(pu, size.get(pu) + size.get(pv));
-            }
-        }
-    }
-}
 class Solution {
-    public int findCircleNum(int[][] adj) {
-        int V =adj.length;
-        Disjoint ds =  new Disjoint(V);
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(adj[i][j] == 1)
-                ds.unionBySize(i,j);
+    public void dfs(int node,ArrayList<ArrayList<Integer>> list, boolean vis[]){
+        vis[node] = true;
+        for(int i: list.get(node)){
+            if(vis[i] == false)
+            dfs(i, list, vis);
+        }
+    }
+    public int findCircleNum(int[][] ar) {
+        int n= ar.length;
+        ArrayList<ArrayList<Integer>> list= new ArrayList<>();
+        for(int i=1;i<=n;i++)
+            list.add(new ArrayList<>());
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(ar[i][j]==1)
+                list.get(i).add(j);
             }
         }
+
+        boolean vis[] = new boolean[n];
         int cnt =0;
-        for(int i=0;i<V;i++){
-            if(ds.parent.get(i) == i)cnt++;
-        }
-        return cnt;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i,list,vis);
+            }
+        }return cnt;
     }
 }
