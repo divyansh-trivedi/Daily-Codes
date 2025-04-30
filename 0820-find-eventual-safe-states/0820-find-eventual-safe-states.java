9ab90489194693ca363f53bd35e663baf4a48308
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-04-30 22:40:31 +0530

    Time: 23 ms (44.27%), Space: 55.4 MB (74.31%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,48 +1,32 @@
 class Solution {
-    public boolean dfs(int pathVis[],int node , int vis[], ArrayList<ArrayList<Integer>> adj, int check[]){
-                vis[node] =1;
-                pathVis[node] =1;
-                check[node] =0;
-        
-                for(int i: adj.get(node)){
-                    if(vis[i] == 0){
-                        if(dfs(pathVis, i, vis, adj, check)==true)return true;
-                    }else if (pathVis[i] == 1) return true;
-                }
-                check[node] = 1;
-                pathVis[node] = 0;
-                return false;
-            }
-        
-          
-            public List<Integer> safeNodes( ArrayList<ArrayList<Integer>> adj) {
-               int v = adj.size();
-               int vis[] = new int[v];
-               int pathVis[] = new int[v];
-               int check[] = new int[v];
-               List<Integer> list = new ArrayList<>();
-        
-               for (int i = 0; i < v; i++) {
-                if (vis[i] == 0) 
-                    dfs(pathVis, i, vis, adj, check);
-               }
-        
-               for(int i=0;i<v;i++){
-                if(check[i] == 1)list.add(i);
-               }
-               return list;
-            }
-        
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < graph.length; i++) 
-        list.add(new ArrayList<>()); 
+        Queue<Integer> q = new LinkedList<>();
+        int n= graph.length;
+        ArrayList<ArrayList<Integer>> reverseGraph = new ArrayList<>();
+        ArrayList<Integer> topo = new ArrayList<>();
+        int outd[] = new int[n];
+        
+        for(int i=0;i<n;i++)reverseGraph.add(new ArrayList<>());
 
-        for(int i=0;i<graph.length;i++){
-            for(int j=0;j<graph[i].length;j++)
-                list.get(i).add(graph[i][j]);
+        for(int i=0;i<n;i++){
+            for(int it :graph[i])
+                reverseGraph.get(it).add(i);
+            outd[i] = graph[i].length;
+            if(outd[i] == 0)
+            q.add(i);
         }
-        return safeNodes(list);
+
+        while(!q.isEmpty()){
+            int rem = q.poll();
+            topo.add(rem);
+
+            for(int it:reverseGraph.get(rem)){
+                outd[it]--;
+                if(outd[it] == 0)
+                q.add(it);
+            }
+        }
+        Collections.sort(topo);
+        return topo;
     }
-    
 }
