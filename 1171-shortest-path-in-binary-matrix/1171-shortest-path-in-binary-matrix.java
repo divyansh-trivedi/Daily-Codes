049e884cefdcec866c0132bf4da3b2662f8ea1fc
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-07-16 19:37:59 +0530

    Time: 15 ms (71.94%), Space: 45.5 MB (75.44%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,33 +1,31 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
-
         int n = grid.length;
-        int m = grid[0].length;
-         if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        int m= grid[0].length;
+        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        q.add(new int[]{0,0});
+        int dist[][] = new int[n][m];
 
-        int dist[][]= new int[n][m];
-        int dx[] = {-1,-1,-1,0,0,1,1,1};
-        int dy[] = {-1,0,1,1,-1,-1,0,1};
-
-        for(int i[]: dist)Arrays.fill(i,(int)1e9);
+        for(int i[] : dist)Arrays.fill(i, (int)1e9);
         dist[0][0] = 1;
+
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
 
         while(!q.isEmpty()){
             int ar[] = q.poll();
             int r = ar[0];
             int c = ar[1];
+            
+            if (r == n-1 && c == m-1) return dist[r][c]; 
+            for(int k=0;k<8;k++){
+                int newr = r+ dr[k];
+                int newc = c+ dc[k];
 
-            if(r==n-1&& c==m-1)return dist[r][c];
-
-            for(int i=0;i<8;i++){
-                int newR = dx[i]+r;
-                int newC = dy[i]+c;
-
-                if(newR>=0&& newC>=0 && newR<n && newC<m && grid[newR][newC] == 0&& dist[newR][newC] > 1+dist[r][c]){
-                    dist[newR][newC] = 1+dist[r][c];
-                    q.add(new int[]{ newR, newC});
+                if(newr>=0&& newc>=0 && newr<n && newc<m && grid[newr][newc] == 0&& dist[newr][newc] > 1+dist[r][c]){
+                    dist[newr][newc] = 1+dist[r][c];
+                    q.add(new int[]{ newr, newc});
                 }
             }
         }
