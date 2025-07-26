d2763b9bbb669844aa9b4ee9dc88900768d315d6
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-07-26 17:05:16 +0530

    Time: 0 ms (100%), Space: 27.7 MB (20.32%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,32 +1,31 @@
+
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         map<int, int> inmap;
-        for (int i = 0; i < inorder.size(); i++)
-            inmap[inorder[i]] = i;
+        for(int i=0;i<inorder.size();i++)
+        inmap[inorder[i]] = i;
 
-        return solve(preorder, 0, preorder.size() - 1,
-                     inorder, 0, inorder.size() - 1, inmap);
+        return solve(preorder , 0,preorder.size()-1,inorder,0,inorder.size()-1,inmap);
+        
     }
-
 private:
-    TreeNode* solve(vector<int>& pre, int pres, int pree,
-                    vector<int>& in, int ins, int ine,
-                    map<int, int>& inmap) {
+    TreeNode* solve(vector<int>& pre, int pres, int pree, 
+                    vector<int>& in , int ins, int ine,
+                     map<int, int>& inmap){
 
-        if (pres > pree || ins > ine)
-            return NULL;
+        if(ins>ine || pres>pree)
+        return NULL;
 
         TreeNode* root = new TreeNode(pre[pres]);
+        
         int inRoot = inmap[root->val];
         int numsLeft = inRoot - ins;
 
-        root->left = solve(pre, pres + 1, pres + numsLeft,
-                           in, ins, inRoot - 1, inmap);
-
-        root->right = solve(pre, pres + numsLeft + 1, pree,
-                            in, inRoot + 1, ine, inmap);
+        root->left = solve(pre,pres+1,pres+numsLeft , in , ins,inRoot-1, inmap);
+        root->right = solve(pre,pres+numsLeft+1,pree , in , inRoot+1,ine, inmap);
 
         return root;
+
     }
 };
