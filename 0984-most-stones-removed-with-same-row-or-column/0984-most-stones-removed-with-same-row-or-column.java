2615506c817893ae837379a03ee3264d1560fb77
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-08-18 23:05:08 +0530

    Time: 18 ms (59.52%), Space: 45 MB (72.83%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,59 +1,81 @@
-class Disjoint {
-    List<Integer> size = new ArrayList<>(), parent = new ArrayList<>();
+class Disjoint{
+   List<Integer> size = new ArrayList<>();
+   List<Integer> parent = new ArrayList<>();
+   public Disjoint(int n){
+    for(int i=0;i<=n;i++){
+        size.add(0);
+        parent.add(i);
+     }
+   }
+   public int findParent(int node){
+    if(node == parent.get(node))
+    return node;
 
-    public Disjoint(int n) {
-        for (int i = 0; i < n; i++) {
-            size.add(1);
-            parent.add(i);
-        }
-    }
+    int ultimate_parent = findParent(parent.get(node));
+    parent.set(node, ultimate_parent);
+    return parent.get(node);
+   }
 
-    public int findParent(int node) {
-        if (node == parent.get(node)) return node;
-        parent.set(node, findParent(parent.get(node)));
-        return parent.get(node);
-    }
+   public void unionBySize(int u, int v){
+    int ultimate_parent_u = findParent(u);
+    int ultimate_parent_v = findParent(v);
 
-    public void unionBySize(int u, int v) {
-        int pu = findParent(u), pv = findParent(v);
-        if (pu != pv) {
-            if (size.get(pu) < size.get(pv)) {
-                parent.set(pu, pv);
-                size.set(pv, size.get(pu) + size.get(pv));
-            } else {
-                parent.set(pv, pu);
-                size.set(pu, size.get(pu) + size.get(pv));
-            }
-        }
+    if(ultimate_parent_u == ultimate_parent_v)return;
+
+    if(size.get(ultimate_parent_u) > size.get(ultimate_parent_v)){
+        parent.set(ultimate_parent_v,ultimate_parent_u);
+        size.set(ultimate_parent_u, size.get(ultimate_parent_v)+size.get(ultimate_parent_u));   
+    }else{
+        parent.set(ultimate_parent_u, ultimate_parent_v);
+        size.set(ultimate_parent_v,size.get(ultimate_parent_v)+size.get(ultimate_parent_u ));
     }
+   }
 }
-
 class Solution {
     public int removeStones(int[][] stones) {
-        int n= stones.length;
-        int row = 0 , col = 0;
-        for(int[] i: stones){
-            row = Math.max(row, i[0]);
-            col = Math.max(col , i[1]);
+        int n = stones.length;
+        int maxr = 0, maxc = 0;
+        for(int it[]:stones){
+            maxr = Math.max(maxr, it[0]);
+            maxc = Math.max(maxc, it[1]);
         }
-        Disjoint ds = new Disjoint(row+col+2);
+        Disjoint ds = new Disjoint(maxr + maxc+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int it[] : stones){
+            int r = it[0];
+            int c = it[1] + maxr + 1;
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++){
-            int nodeRow = stones[i][0];
-            int nodeCol = stones[i][1] + row + 1;
-
-            ds.unionBySize(nodeRow, nodeCol);
-
-            map.put(nodeRow,1);
-            map.put(nodeCol,1);
+            ds.unionBySize(r, c);
+            map.put(r,1);
+            map.put(c,1);
         }
-
         int cnt = 0;
-        for(Map.Entry<Integer, Integer> it : map.entrySet()){
-            int key = it.getKey();
-            if(ds.findParent(key) == key)cnt++;
+        for(int it: map.keySet()){
+            if(ds.findParent(it) == it)
+            cnt++;
         }
-        return n - cnt;
+        return n-cnt;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
