995eab9006906b7c5652e7dd6316823d899707b5
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-08-31 15:57:17 +0530

    Time: 1200 ms (16.67%), Space: 468.6 MB (0%) - LeetHub

--- 3938-twisted-mirror-path-count/3938-twisted-mirror-path-count.cpp
+++ 3938-twisted-mirror-path-count/3938-twisted-mirror-path-count.cpp
@@ -1,35 +1,33 @@
 class Solution {
-public:
-    int MOD = 1e9 + 7;
-    bool valid(vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size()) {
+    static const int mod = 1e9+7; 
+    vector<vector<vector<int>>> dp; 
+private:
+    int solve(vector<vector<int>>& grid , int r, int c, int prev){
+        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size())
             return 0;
-        }
-        return 1;
-    }
-    long long solve(vector<vector<int>>& grid, int i, int j, char dir,
-                    vector<vector<long long>>& dp) {
-        if (!valid(grid, i, j))
-            return 0;
-        if (dp[i][j] != -1)
-            return (dp[i][j]);
-        if (i == grid.size() - 1 && j == grid[0].size() - 1)
+        if(r == grid.size()-1 && c == grid[0].size()-1)
             return 1;
 
-        if (dir == 'L' && grid[i][j] == 1)
-            return solve(grid, i + 1, j, 'U', dp);
-        if (dir == 'U' && grid[i][j] == 1)
-            return solve(grid, i, j + 1, 'L', dp);
-        return dp[i][j] = ((solve(grid, i + 1, j, 'U', dp)) % MOD +
-                           (solve(grid, i, j + 1, 'L', dp)) % MOD) %
-                          MOD;
-    }
+        int temp = (prev == -1 ? 0 : (prev == 0 ? 1 : 2));
+        if(dp[r][c][temp] != -1) 
+            return dp[r][c][temp];
 
+        int right = 0, down = 0;
+        if(grid[r][c] == 0){
+            right += solve(grid, r, c+1, 0);
+            down  += solve(grid, r+1, c, 1);
+        } else {
+            if(prev == 0)
+                down += solve(grid, r+1, c, 1);
+            else 
+                right += solve(grid, r, c+1, 0);
+        }
+        return dp[r][c][temp] = (right+down)%mod; 
+    }
+public:
     int uniquePaths(vector<vector<int>>& grid) {
-        char dir = ' ';
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<long long>> dp(m, vector<long long>(n, -1));
-        return (int)(solve(grid, 0, 0, dir, dp)) % MOD;
+        int n = grid.size(), m = grid[0].size();
+        dp = vector<vector<vector<int>>>(n, vector<vector<int>>(m, vector<int>(3, -1)));
+        return solve(grid, 0, 0, -1);
     }
 };
