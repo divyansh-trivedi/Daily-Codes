dcc8ae3db282ab0124fd2ba86754aa2e404e0e4a
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-09-10 15:13:27 +0530

    Time: 0 ms (100%), Space: 26.1 MB (11.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        return multiset<char>(s.begin(), s.end()) == multiset<char>(t.begin(), t.end());
+    }
+};
+ auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
