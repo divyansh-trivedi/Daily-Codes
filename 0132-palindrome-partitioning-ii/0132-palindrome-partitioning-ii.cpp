0b13fc01e47c8c68ca4798d2281c142450fe64a7
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-09-27 21:52:20 +0530

    Time: 1283 ms (50.21%), Space: 9.4 MB (72.03%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    bool isPali(string &s, int i, int j){
+    bool isPalindrome(string &s, int i, int j){
         while(i<j){
-            if(s[i]!=s[j])return 0;
-            i++;
-            j--;
+            if(s[i]!=s[j]) return false;
+            i++; j--;
         }
-        return 1;
+        return true;
+    }
+    int solve(string &s, int i,vector<int> &dp){
+        if(i==s.length()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int minCost=INT_MAX;
+        for(int j=i;j<s.length();j++){
+            if(isPalindrome(s,i,j)){
+                int cost=1+solve(s,j+1,dp);
+                minCost=min(minCost,cost);
+            }
+        }
+        return dp[i]=minCost;
     }
     int minCut(string s) {
-        int n=s.size();
-        vector<int> dp(n+1,0);
-
-        for(int i=n-1;i>=0;i--){
-            int mini = 1e9;
-            for(int j=i;j<n;j++){
-                if(isPali(s,i,j)){
-                    int count = 1+dp[j+1];
-                    mini = min(mini , count);
-                }
-            }
-            dp[i] =  mini;
-        }
-
-        return dp[0]-1;
+        vector<int> dp(s.length()+1,-1);
+        return solve(s,0,dp)-1;
     }
 };
