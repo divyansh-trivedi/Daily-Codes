0adde80d48d149f43d23367e1f1e955cec1000a2
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-09-28 00:10:29 +0530

    Time: 14 ms (56.71%), Space: 47.9 MB (20.52%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,49 +1,54 @@
-class Pair{
-    int row,  col;
-    int val;
-    Pair(int r , int c, int v){
-        row =r;
-        col=c;
-        val=v;
+
+ class Pair{
+    int row;
+    int col;
+    int value;
+
+    Pair(int r, int c, int v){
+        row = r;
+        col = c;
+        value = v;
     }
-}
-class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        int n= mat.length;
-        int m= mat[0].length;
-        Queue<Pair> q= new LinkedList<>();
-        int ans[][] = new int[n][m];
+ }
+    class Solution {
+    public int[][] updateMatrix(int[][] grid) {
+       int n= grid.length , m = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        int [][] ans = new int[n][m];
         boolean vis[][] = new boolean[n][m];
-
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
+            for(int j=0;j<m;j++)
+                if(grid[i][j] == 0){
                     q.add(new Pair(i,j,0));
+                    vis[i][j] = true;
                 }
-            }
         }
-
-        while(!q.isEmpty()){
-            Pair rem= q.poll();
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        while (!q.isEmpty()) {
+            Pair rem = q.poll();
             int r = rem.row;
             int c = rem.col;
-            int val = rem.val;
-            int dx[] ={-1,0,1,0};
-            int dy[] ={0,1,0,-1};
+            int v = rem.value;
+            ans[r][c] = v;
 
             for(int k=0;k<4;k++){
-                int nr = r+dx[k];
-                int nc = c+dy[k];
-
-                if(nr>=0 &&nc>=0&&nr<n&&nc<m){
-                    if(vis[nr][nc] == false && mat[nr][nc] == 1){
-                        q.add(new Pair(nr, nc, val+1));
-                        ans[nr][nc] = val+1;
-                        vis[nr][nc]= true;
-                    }
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                
+                if(nr>=0&&nr<n && nc>=0 && nc<m && !vis[nr][nc]){
+                    if(grid[nr][nc] == 1){
+                        q.add(new Pair(nr, nc, v+1)); 
+                        vis[nr][nc] = true;
+                        ans[nr][nc] = v+1;
+                     }
                 }
             }
         }
         return ans;
     }
-}
+    
+ }
+ 
+
