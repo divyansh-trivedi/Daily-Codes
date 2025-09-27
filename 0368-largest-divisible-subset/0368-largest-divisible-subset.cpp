48615cc6f4744c82aa9ff37de7fc9d9df1faadc5
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-09-27 15:40:52 +0530

    Time: 12 ms (42.52%), Space: 12.4 MB (99%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1), hash(n);
-        sort(nums.begin(), nums.end());
-        int maxi = 1, lastIdx = 0;
-
-        for(int idx =0;idx<n;idx++){
-            hash[idx] = idx;
-            for(int prev =0;prev<idx;prev++){
-                if(nums[idx] % nums[prev] == 0 && dp[prev]+1 > dp[idx]){
-                    dp[idx] = dp[prev] +1;
-                    hash[idx] = prev;
+        vector<int> dp(n,1) , hash(n,1);
+        sort(nums.begin(),nums.end());
+        int maxi = 1;
+        int idx =-1;
+        for(int i=0;i<n;i++){
+            hash[i] = i;
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]%nums[prev] == 0 && dp[prev]+1 > dp[i] ){
+                    dp[i] = dp[prev]+1;
+                    hash[i]= prev;
                 }
             }
-            if(maxi < dp[idx]){
-                maxi = dp[idx];
-                lastIdx = idx;
-            }
+            if(idx == -1 || dp[i]>maxi){
+                maxi = dp[i];
+                idx = i;
+            } 
         }
-        cout << "DP Array: ";
-        for(int i = 0; i < n; i++)
-            cout << dp[i] << " ";
-        cout << endl;
         vector<int> temp;
-        temp.push_back(nums[lastIdx]);
-        while(lastIdx != hash[lastIdx]){
-            lastIdx = hash[lastIdx];
-            temp.push_back(nums[lastIdx]);
+        temp.push_back(nums[idx]);
+        while(hash[idx] != idx){
+            idx =  hash[idx];
+            temp.push_back(nums[idx]);
         }
-        reverse(temp.begin(), temp.end());
+        reverse(temp.begin(),temp.end());
         return temp;
     }
 };
