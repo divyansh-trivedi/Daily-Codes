33ba39088bec0d5fa48bdc543e2f8c9cc760f935
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-10-02 15:13:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.66%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drunk = numBottles;    
+        int empty = numBottles;    
+        while(empty >= numExchange){
+            empty -= numExchange;   
+            numExchange++;         
+            drunk++;                
+            empty++;             
+        }
+        return drunk;
+    }
+};
