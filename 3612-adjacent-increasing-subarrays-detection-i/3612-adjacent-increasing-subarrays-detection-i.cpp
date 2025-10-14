69fe1b18278f8c95a4da4e8d104eeddcd4f55d80
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-10-14 18:50:18 +0530

    Time: 11 ms (76.11%), Space: 40.4 MB (53.98%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -5,7 +5,6 @@ public:
     bool hasIncreasingSubarrays(std::vector<int>& nums, u32 k) noexcept
     {
         const u32 n = static_cast<u32>(nums.size());
-
         u32 i = 0, j = i + k, l = 0;
         int prev_i = -1, prev_j = -1;
         while (i != n - k && j != n && l != k)
