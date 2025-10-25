aa6eec3feb21b1e8048637f6e467b8e3d74c52a8
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-10-25 18:43:51 +0530

    Time: 5 ms (57.5%), Space: 45.3 MB (18.88%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int minDistance(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        int dp[][] = new int[n+1][m+1];
+    public int minDistance(String w1, String w2) {
+        int n = w1.length() , m =  w2.length();
 
+        int dp[][] = new int[n+1][m+1];
         for(int i=0;i<=n;i++)dp[i][0] = i;
-        for(int j=0;j<=m;j++)dp[0][j] = j;
-        
+        for(int i=0;i<=m;i++)dp[0][i] = i;
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1))
+                if(w1.charAt(i-1) == w2.charAt(j-1))
                 dp[i][j] = dp[i-1][j-1];
-                else 
-                dp[i][j] = Math.min(dp[i-1][j-1],Math.min( dp[i-1][j], dp[i][j-1]))+1;
+                else
+                dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
             }
         }
         return dp[n][m];
