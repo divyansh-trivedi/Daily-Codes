962d4377cec927a345a39bee835acd954d032d72
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-11-05 22:44:35 +0530

    Time: 0 ms (100%), Space: 40.6 MB (40.87%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,10 +1,13 @@
 class Solution {
     public int numTrees(int n) {
-    if(n <= 1) return 1;
-    int res = 0;
-    for(int i = 0; i < n; i++) {
-        res += numTrees(i) * numTrees(n - 1 - i);          
+        int dp[] = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int level = 2; level <=n;level++){
+            for(int root = 1;root<=level;root++)
+            dp[level] += dp[level - root]*dp[root-1];
+        }
+        return dp[n];
     }
-    return res;      
-}
 }
