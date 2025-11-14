e44ae5a69da621f241d010b59501795c0437f4cd
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-11-14 23:01:42 +0530

    Time: 39 ms (52.04%), Space: 86.5 MB (75.24%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -3,10 +3,19 @@ public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
         vector<vector<int>> ans(n,vector<int>(n,0));
 
-        for(auto& it :  queries){
-            for(int i=it[0];i<=it[2];i++){
-                for(int j=it[1];j<=it[3];j++)
-                    ans[i][j] += 1;
+        for(auto& it : queries){
+            int r1 = it[0] , c1 = it[1] , r2 = it[2] , c2 = it[3];
+
+            for(int i=r1;i<=r2;i++){
+                ans[i][c1] += 1;
+                if(c2+1 < n)
+                ans[i][c2+1] -= 1;
+            } 
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                ans[i][j] += ans[i][j-1];
             }
         }
         return ans;
@@ -14,7 +23,8 @@ public:
 };
 
 
-/* 
+/*
+
 1 1 0
 1 2 1
 0 1 1
@@ -22,6 +32,39 @@ public:
 
 [[1,1,2,2],[0,0,1,1]]
 
+[1,1->2,2]
+
 o(n) > 10^8
 o(nlog(n)) >10^4 && <=10^8
-o(n^2) <=10^4 */
+o(n^2) <=10^4 
+
+
+Array difference technique 1-d -:
+
+queries = [[1,1,2,2],[0,0,1,1]]
+
+1 1 0 0
+1 2 1 0
+0 1 1 0
+0 0 0 0
+
+[1,1,2,2] ->  r1 = 1, c1 =1 , r2 =2, c2=2
+
+
+for(int i=r1;i<=r2;i++){
+    ans[i][c1] += 1;
+    if(c2+1 < n)
+    ans[i][c2+1] = -1;
+}
+
+1 0 -1 0   => 1 1 0 0
+1 1 -1 -1     1 2 1 0
+0 1 0 -1      0 1 1 0
+0 0 0 0       0 0 0 0
+
+[1,1,2,2] ->  r1 = 1, c1 =1 
+              r2 =2 , c2=2
+
+[0,0,1,1] ->  r1 = 0, c1 =0 
+              r2 =1 , c2=1
+*/
