f17098f35f1e0b0763f92528a8f607584f36991d
Divyansh Trivedi <divyanshtrivedi85@gmail.com>
2025-11-28 02:58:54 +0530

    Time: 78 ms (81.33%), Space: 48.5 MB (97.42%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        int dp[n][m][k];
-        memset(dp, 0, sizeof(dp));
-        
-        dp[0][0][grid[0][0] % k] = 1;
-        
-        int mod = 1e9 + 7;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                for(int K = 0; K < k; K++) {
-                    int sum = (K + grid[i][j]) % k;
-                    if(i) dp[i][j][sum] += dp[i - 1][j][K] % mod;
-                    if(j) dp[i][j][sum] += dp[i][j - 1][K] % mod;
+        int  n = grid.size() ;
+        int  m = grid[0].size();
+        int dp[n] [m][k];
+        memset(dp,0,sizeof(dp));
+
+        dp[0][0][grid[0][0]%k] = 1;
+        int MOD = 1e9+7;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                for(int r=0; r<k; r++){
+                    int cur = dp[i][j][r];
+                    if (cur == 0) continue;
+
+                    // move down
+                    if (i+1 < n) {
+                        int nr = (r+grid[i+1][j]) % k;
+                        dp[i + 1][j][nr] = (dp[i+1][j][nr]+cur) % MOD;
+                    }
+
+                    // move right
+                    if (j+1< m) {
+                        int nr = (r + grid[i][j+1]) % k;
+                        dp[i][j+1][nr] = (dp[i][j+1][nr]+cur) % MOD;
+                    }
                 }
             }
         }
-        
-        return dp[n - 1][m - 1][0] % mod;
+        return dp[n-1][m-1][0]%MOD;
     }
 };
